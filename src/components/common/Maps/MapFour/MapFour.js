d531520ef8b3e13d33752d5792fb6393c49ca325
Guillermo S <gsantaolalla2006@gmail.com>
2021-02-02 11:54:59 -0600

    More stuff

--- src/components/common/Maps/MapFour/MapFour.js
+++ src/components/common/Maps/MapFour/MapFour.js
@@ -1,12 +1,18 @@
 import React from "react";
 import BackArrow from "./../../BackArrow";
 import Header from "./../../Header";
+import style from "./MapFour.module.css";
+
+import MapFourImage from "./../../../../images/MapFour.png";
+import InsideSwitchButton from "./../../InsideSwitchButton";
 
 function MapFour() {
   return (
     <div>
       <Header content={"Carte 4"} />
       <BackArrow />
+      <img className={style.image} src={MapFourImage} alt="MapFour" />
+      <InsideSwitchButton content={"Jour 4"} link={"/Jour/4"} />
     </div>
   );
 }
